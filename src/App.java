fa00d8dc19eea6febb535b0de927cf402dcfc6f3
13111586 <ial.silva@acad.pucrs.br>
2017-03-30 20:19:43 -0300

    implementação fase I

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 
 public class App {
-
+	public static void main(String[] args) {
+		Cliente cl= new Cliente(1, "nome", 1, 'm', false,0,0);
+		
+		System.out.println(cl.toString());
+	}
 }
--- src/Cliente.java
+++ src/Cliente.java
@@ -1,6 +1,6 @@
 
 public class Cliente {
-	
+
 	private int cpf;
 	private String nome;
 	private int idade;
@@ -8,25 +8,32 @@ public class Cliente {
 	private boolean socio;
 	private int codigoDoSocio;
 	private int milhagem;
-	
-	public Cliente(int cpf, String nome, int idade, char genero, boolean socio){
-		this.cpf=cpf;
-		this.nome=nome;
-		this.idade=idade;
-		this.genero=genero;
-		this.socio=socio;
-		if (socio==true){
-			cadastraMilhagem(codigoDoSocio, milhagem);
+
+	public Cliente(int cpf, String nome, int idade, char genero, boolean socio, int codigoDoSocio, int mi) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.idade = idade;
+		this.genero = genero;
+		this.socio = socio;
+		if (socio == true) {
+			cadastraMilhagem(codigoDoSocio, mi);
 		}
 	}
-	
-	public void cadastraMilhagem(int codigoDoSocio, int milhagem){
-		this.codigoDoSocio=codigoDoSocio;
-		this.milhagem=milhagem;
+
+	public void cadastraMilhagem(int codigoDoSocio, int milhagem) {
+		this.codigoDoSocio = codigoDoSocio;
+		this.milhagem = milhagem;
 	}
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		if (this.socio == true) {
+			return "Cliente [cpf=" + cpf + ", nome=" + nome + ", idade=" + idade + ", genero=" + genero + ", socio="
+					+ socio + ", codigoDoSocio=" + codigoDoSocio + ", milhagem=" + milhagem + "]";
+
+		} else
+			return "Cliente: \nCpf=" + cpf + "\nNome=" + nome + ", idade=" + idade + ", genero=" + genero;
+
+	}
 
 }
